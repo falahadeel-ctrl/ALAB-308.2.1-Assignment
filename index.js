1e4666057f5981f013229a604f2e0aaa3c4c4ee0
falahadeel-ctrl <falahadeel@gmail.com>
2025-12-03 19:18:41 -0500

    first

---
+++ index.js
@@ -0,0 +1,31 @@
+const PI = 3.1415;
+const total_radius = 5;
+const total_area = PI * total_radius * total_radius;
+
+let plant_area = 0.8;
+
+let plantsPerWeek = beginingPlans * 2 ** week
+// Predict the plant growth after a specific number of weeks.
+let week = 1
+let weekly_growth = 20*2**week;
+
+if (week == 1){
+   console.log(weekly_growth) ;
+}
+else if(week == 2){
+    console.log(weekly_growth);
+}
+else if (week == 3){
+    console.log(weekly_growth);
+}
+// Implement control flow to make decisions on whether the plants should be:
+// Pruned, to stop them from exceeding the capacity of the garden.
+// This condition should be met if the plant count after the given number of weeks is greater than 80% of the maximum capacity of the garden.
+// Monitored, if they are growing at an acceptable rate.
+// This condition should be met if the plant count is between 50% and 80% of the maximum capacity of the garden after the given number of weeks.
+// Planted, if there is room to plant more plants.
+// This condition should be met if the plant count after the given number of weeks is less than 50% of the maximum capacity of the garden.
+
+plants = (plants + init_plants)  2*weeks;
+plants = (plants + init_plants)  2**weeks;
+plants = (plants + init_plants)  * 2**weeks;
